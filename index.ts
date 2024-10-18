31f4c2afaadc9487f086cc6f28f27cb902d430ae
mbernasinski <mbernasinski@hoppr.tech>
2024-10-18 15:32:10 +0200

    docs: Adding CLI command description and summary

--- index.ts
+++ index.ts
@@ -3,6 +3,10 @@ import { generatorHandler } from './src/command.handler.ts';
 
 export const generateGrapher = new Command('generate-grapher')
     .alias('gg')
-    .requiredOption('-n, --name <string>', 'name of the grapher project')
-    .requiredOption('-od, --output-dir <string>', 'output dir for the grapher project')
+    .summary('generates a new grapher project')
+    .description(
+        'Generates a new grapher project.\nBased on a standard dragee grapher template, with mandatory dependancies and a sample graph.'
+    )
+    .requiredOption('-n, --name <string>', 'name of the new grapher project')
+    .requiredOption('-od, --output-dir <string>', 'output dir for the new grapher project')
     .action(generatorHandler);
